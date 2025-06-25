042578e9ed9f2ff2dcd4932d841a9d988445520e
KristofferGW <kristoffer.wallqvist@gmail.com>
2025-06-25 20:49:31 +0200

    Integrated Portal by Wormhole

--- src/services/bridgeApi.ts
+++ src/services/bridgeApi.ts
@@ -1,56 +1,85 @@
-export const fetchBungeeChains = async () => {
-    const res = await fetch("https://public-backend.bungee.exchange/api/v1/supported-chains");
-    if (!res.ok) throw new Error("Failed to fetch Bungee chains");
-    const data = await res.json();
+import { wormhole } from "@wormhole-foundation/sdk";
+import evm from '@wormhole-foundation/sdk/evm';
+import solana from '@wormhole-foundation/sdk/solana';
+import sui from '@wormhole-foundation/sdk/sui';
+import { nativeChainIdMap } from "../constants/nativeChainIdMap";
 
-    return data.result
-        .filter((chain: any) => chain.sendingEnabled && chain.receivingEnabled)
-        .map((chain: any) => ({
-            name: chain.name,
-            chainId: chain.chainId,
-            icon: chain.icon,
-            source: "Bungee"
-        }));
+export const fetchBungeeChains = async () => {
+  const res = await fetch("https://public-backend.bungee.exchange/api/v1/supported-chains");
+  if (!res.ok) throw new Error("Failed to fetch Bungee chains");
+  const data = await res.json();
+
+  return data.result
+    .filter((chain: any) => chain.sendingEnabled && chain.receivingEnabled)
+    .map((chain: any) => ({
+      name: chain.name,
+      chainId: nativeChainIdMap[chain.name] ?? chain.chainId,
+      source: "Bungee"
+    }));
 };
 
 export const fetchDebridgeChains = async () => {
-    const res = await fetch("https://dln.debridge.finance/v1.0/supported-chains-info");
-    if (!res.ok) throw new Error("Failed to fetch deBridge chains");
-    const data = await res.json();
+  const res = await fetch("https://dln.debridge.finance/v1.0/supported-chains-info");
+  if (!res.ok) throw new Error("Failed to fetch deBridge chains");
+  const data = await res.json();
 
-    return data.chains.map((chain: any) => ({
-      name: chain.chainName,
-      chainId: chain.originalChainId,
-      icon: null,
-      source: "deBridge"
-    }));
+  return data.chains.map((chain: any) => ({
+    name: chain.chainName,
+    chainId: nativeChainIdMap[chain.chainName] ?? chain.originalChainId,
+    source: "deBridge"
+  }));
 };
 
 export const fetchJumperChains = async () => {
-    const res = await fetch("https://li.quest/v1/chains");
-    if (!res.ok) throw new Error("Failed to fetch Jumper chains");
-    const data = await res.json();
+  const res = await fetch("https://li.quest/v1/chains");
+  if (!res.ok) throw new Error("Failed to fetch Jumper chains");
+  const data = await res.json();
 
-    return data.chains.map((chain: any) => ({
-        name: chain.name,
-        chainId: chain.id ?? null,
-        icon: chain.logoURI ?? "",
-        source: "Jumper"
-    }));
+  return data.chains.map((chain: any) => ({
+    name: chain.name,
+    chainId: chain.id,
+    source: "Jumper"
+  }));
+};
+
+export const fetchPortalChains = async () => {
+  const wh = await wormhole("Mainnet", [evm, solana, sui]);
+
+  const chains = Object.entries(wh.config.chains).map(([name, _chainCtx]) => {
+    if (typeof _chainCtx.nativeChainId === "bigint") {
+      return {
+        name: name,
+        chainId: Number(_chainCtx.nativeChainId),
+        source: "Portal"
+      };
+    } else if (typeof _chainCtx.nativeChainId === "string") {
+      return {
+        name: name,
+        chainId: _chainCtx.nativeChainId,
+        source: "Portal"
+      };
+    } else {
+      return null;
+    }
+  });
+
+  return chains.filter(Boolean); // filtrerar bort null om nån chain inte matchade
 };
 
 export const fetchStargateChains = async () => {
-    const res = await fetch("https://stargate.finance/api/v1/chains");
-    if (!res.ok) throw new Error("Failed to fetch Stargate chains");
-    const data = await res.json();
+  const res = await fetch("https://stargate.finance/api/v1/chains");
+  if (!res.ok) throw new Error("Failed to fetch Stargate chains");
+  const data = await res.json();
 
-    return data.chains.map((chain: any) => ({
-        name: chain.name,
-        chainId: chain.chainId ?? null,
-        icon: "",
-        source: "Stargate"
-    }));
+  return data.chains.map((chain: any) => ({
+    name: chain.name,
+    chainId: chain.chainId ?? null,
+    icon: "",
+    source: "Stargate"
+  }));
 };
+
+console.log(await fetchPortalChains());
 
 export const bridgesConfig = [
   {
@@ -70,6 +99,12 @@ export const bridgesConfig = [
     url: "https://www.jumper.exchange",
     fetchChains: fetchJumperChains,
     sourceName: "Jumper",
+  },
+  {
+    name: "Portal",
+    url: "https://portalbridge.com",
+    fetchChains: fetchPortalChains,
+    sourceName: "Portal",
   },
   {
     name: "Stargate Finance",
